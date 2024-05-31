a55b45a80ce9ddf8a4e363b54a742d71efa6000d
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-05-31 19:12:24 +0200

    use action

--- src/components/aside-menu.tsx
+++ src/components/aside-menu.tsx
@@ -1,28 +1,11 @@
 import { LogoIcon, PlusIcon } from '@/icons'
-import { MY_EMAIL_KEY, SIDE_MENU_ITEMS } from '@/constants'
+import { SIDE_MENU_ITEMS } from '@/constants'
 import { AsideItem } from '@/components/aside-item'
-import { gql } from 'graphql-request'
-import { client } from '@/lib/client'
-import { type MovieList } from '@/types'
 import Link from 'next/link'
-
-const GET_MOVIES_LIST = gql`
-  query GetMovieLists($email: String!) {
-    getMovieLists(email: $email) {
-      id
-      name
-      email
-      created_at
-    }
-  }
-`
+import { getMovieLists } from '@/actions/get-movie-lists'
 
 export default async function AsideMenu() {
-  const { getMovieLists } = await client.request<{
-    getMovieLists: MovieList[]
-  }>(GET_MOVIES_LIST, {
-    email: MY_EMAIL_KEY,
-  })
+  const movieLists = await getMovieLists()
 
   return (
     <div className='flex flex-col flex-1 gap-2'>
@@ -45,11 +28,11 @@ export default async function AsideMenu() {
       <section className='bg-[#080808] rounded-lg p-4 flex-1'>
         <h2 className='text-base text-[#9A9A9A]'>My Lists</h2>
         <div className='mt-5'>
-          {getMovieLists.map((list, index) => (
+          {movieLists.map((list, index) => (
             <Link
               key={index}
               className='flex text-white items-center py-2 px-5 font-medium transition-all duration-300 hover:text-[#FFB320] hover:scale-110'
-              href={`/lists/${list.id}`}
+              href={`/list/${list.id}`}
             >
               {list.name}
             </Link>
