2b0168f3aa5378b5cc384f258ff3d0214191c965
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-06-01 18:18:18 +0200

    Add list card component

---
+++ src/components/list-card.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { type MovieList } from '@/types'
+import Link from 'next/link'
+import { motion } from 'framer-motion'
+
+export const ListCard = ({ list }: { list: MovieList }) => {
+  return (
+    <motion.article
+      key={list.id}
+      initial={{ x: -50, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      whileHover={{
+        scale: 1.1,
+      }}
+      className='w-44 rounded-md bg-gray-600 lg:w-60'
+    >
+      <Link
+        className='flex py-3 text-2xl w-full max-w-44 lg:max-w-60 px-5'
+        href={`/my-lists/${list.id}`}
+      >
+        {list.name}
+      </Link>
+    </motion.article>
+  )
+}
