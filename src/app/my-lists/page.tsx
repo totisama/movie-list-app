dfa440949b34ac1285585c77eb7f82653ce526bc
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-06-01 18:29:39 +0200

    display lists

--- src/app/my-lists/page.tsx
+++ src/app/my-lists/page.tsx
@@ -1,7 +1,21 @@
-export default function ListsPage() {
+import { getMovieLists } from '@/actions/get-movie-lists'
+import { ListCard } from '@/components/list-card'
+
+export default async function ListsPage() {
+  const movieLists = await getMovieLists()
+
   return (
-    <div>
-      <h1>Lists Page</h1>
+    <div className='flex min-h-screen flex-col items-center  pt-10 px-10'>
+      <h1 className='text-6xl text-[#F33F3F]'>My lists</h1>
+      <section className='mt-10 flex justify-center flex-wrap gap-5'>
+        {movieLists.map((list, index) => (
+          <ListCard
+            key={list.id}
+            list={list}
+            index={index}
+          />
+        ))}
+      </section>
     </div>
   )
 }
