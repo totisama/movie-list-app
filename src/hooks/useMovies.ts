639fe156f6a3ebac8118624401235d3de9a27dde
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-06-01 16:45:56 +0200

    Modify hook to get filter by movie "id"

--- src/hooks/useMovies.ts
+++ src/hooks/useMovies.ts
@@ -1,10 +1,14 @@
 import { type ImdbMovie } from '@/types'
 
-export const useMovies = async () => {
+export const useMovies = async ({ id = '' }: { id?: string }) => {
   const res = await fetch(
     'https://raw.githubusercontent.com/theapache64/top250/master/top250_min.json?authuser=1'
   )
-  const movies: ImdbMovie[] = await res.json()
+  let movies: ImdbMovie[] = await res.json()
+
+  if (id !== '') {
+    movies = movies.filter((movie) => movie.imdb_url.includes(`/${id}`))
+  }
 
   return { movies }
 }
