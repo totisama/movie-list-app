43dd85dc085dc807eb241806a0037f26d55cb813
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-06-01 15:40:23 +0200

    Display movies in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
-export default function Home() {
+import { MovieCard } from '@/components/movie-card'
+import { useMovies } from '@/hooks/useMovies'
+
+export default async function Home() {
+  const { movies } = await useMovies()
+
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      Home
+    <main className='flex min-h-screen flex-col items-center justify-between pt-10 px-10'>
+      <h1 className='text-6xl text-[#F33F3F]'>Movies</h1>
+      <section className='mt-10 flex justify-center flex-wrap gap-5'>
+        {movies.map((movie, index) => (
+          <MovieCard
+            key={movie.name}
+            movie={movie}
+            index={index}
+          />
+        ))}
+      </section>
     </main>
   )
 }
