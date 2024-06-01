29ad915aa944eb1c72ea9622d613fad1af7da075
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-06-01 16:47:31 +0200

    send empty object

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { MovieCard } from '@/components/movie-card'
 import { useMovies } from '@/hooks/useMovies'
 
 export default async function Home() {
-  const { movies } = await useMovies()
+  const { movies } = await useMovies({})
 
   return (
     <main className='flex min-h-screen flex-col items-center justify-between pt-10 px-10'>
