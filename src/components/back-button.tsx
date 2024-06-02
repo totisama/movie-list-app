ef4da7ff4116f3ddb8e0e0e1ad4ca7c5b6ac4832
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-06-02 18:40:02 +0200

    change back button behavior

--- src/components/back-button.tsx
+++ src/components/back-button.tsx
@@ -1,12 +1,18 @@
-import Link from 'next/link'
+'use client'
+
+import { useRouter } from 'next/navigation'
 
 export const BackButton = () => {
+  const router = useRouter()
+
   return (
-    <Link
+    <button
       className='block max-w-[60px] bg-gray-700 py-1 px-2 text-center rounded-3xl transition-all duration-400 ease-in-out hover:bg-gray-800 hover:scale-110'
-      href='/'
+      onClick={() => {
+        router.back()
+      }}
     >
       Back
-    </Link>
+    </button>
   )
 }
